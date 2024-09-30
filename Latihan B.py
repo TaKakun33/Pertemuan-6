3c9739a7c0b8863a90bdd2057a22f724768abbea
TaKa <akmalkaflianan33@gmail.com>
2024-09-30 14:05:27 +0700

    idk

--- Latihan B.py
+++ Latihan B.py
@@ -4,7 +4,7 @@ def perkalian3(n):
     else:
        return perkalian3(n-1) + 3
    
-print(perkalian3(5))
+print(perkalian3(2))
 
 def deret(x):
     if x == 1:
@@ -18,6 +18,14 @@ def aritmatika(x):
     if x == 1:
         return 3
     else:
-        return aritmatika(x-1) * x
+        return aritmatika(x-1) + x
     
 print(aritmatika(3))
+
+def geometri(x):
+    if x == 1:
+        return 3
+    else:
+        return geometri(x-1) * x
+    
+print(geometri(3))
